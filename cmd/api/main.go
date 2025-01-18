0d6cb4dee30f2f72d07458919b630a64fa0b37fe
Maksim Fisenko <fisenkomaksim.id@gmail.com>
2025-01-18 20:20:14 +0300

    :star: feat: set up database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -9,6 +11,8 @@ import (
 	"time"
 
 	"github.com/maksimfisenko/goform-server-app/internal/jsonlog"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -16,6 +20,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -28,9 +35,20 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "api server port")
 	flag.StringVar(&cfg.env, "env", "development", "environment (development | staging | production)")
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://goform_user:goform_password@localhost/goform", "postgresql dsn")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
+	defer db.Close()
+
+	logger.PrintInfo("database connection pool established", nil)
 
 	app := &application{
 		config: cfg,
@@ -51,6 +69,23 @@ func main() {
 		"env":  cfg.env,
 	})
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.PrintFatal(err, nil)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, err
 }
